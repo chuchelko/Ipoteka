f95099542e39b52edea2eed54bc7f95ab403bfe4
Эмиль Низамов <emilnizamov9@gmail.com>
2025-06-14 21:21:50 +0300

    gav

--- Program.cs
+++ Program.cs
@@ -86,6 +86,10 @@ async Task HandleUpdateAsync(ITelegramBotClient bot, Update update, Cancellation
                 await ShowHistory(chatId, redis);
                 break;
 
+            case "/gav":
+                await botClient.SendMessage(chatId, "ГАВ");
+                break;
+
             default:
                 await botClient.SendMessage(chatId, "Неизвестная команда. Используйте /start для справки");
                 break;
